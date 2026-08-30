4ae0233ffb224a0033f07137ce26c4535ecc8c6f
Envair <47439360+abenezerendeshaw@users.noreply.github.com>
2026-08-30 18:39:52 +0300

    uploading earlier branch

--- php-backend/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/auth.php';
-require_once __DIR__ . '/includes/helpers.php';
-logout_company();
-redirect(BASE_PATH . '/login.php');
--- php-backend/super-admin/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../includes/auth.php';
-require_once __DIR__ . '/../includes/helpers.php';
-logout_superadmin();
-redirect(BASE_PATH . '/super-admin/login.php');
